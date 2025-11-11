17c580cfd2de8768a6a3de031c0799d6321b24a5
Mike Warren <mwarren04011990@gmail.com>
2025-11-11 14:42:09 -0500

    mantaining consistency of max volume settings state

--- app/src/main/java/com/mikewarren/speakify/data/UserSettingsSerializer.kt
+++ app/src/main/java/com/mikewarren/speakify/data/UserSettingsSerializer.kt
@@ -16,7 +16,7 @@ class UserSettingsSerializer @Inject constructor() : Serializer<UserSettingsMode
         useDarkTheme = true,
         appSettings = emptyMap(),
         selectedTTSVoice = "",
-        maximizeVolumeOnScreenOff = true,
+        maximizeVolumeOnScreenOff = false,
     )
 
     override suspend fun readFrom(input: InputStream): UserSettingsModel {
